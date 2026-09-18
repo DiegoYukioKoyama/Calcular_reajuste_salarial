2408314377e0cb853b865f33509f8cc23ada8e1a
Diego Yukio <yukioko.jp@gmail.com>
2026-09-18 18:00:20 -0300

    feat: finaliza fomatação de saída e adiciona relátorio final de salários

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import funcoes
 
-#Cria a lista de funcionários.
+# Cria a lista de funcionários.
 lista_funcionarios = funcoes.cadastrar_funcionarios()
 
-#Reajusta a lista de funcionários com os novos salários.
-lista_funcionarios_atualizado = funcoes.reajuste_salarial(lista_funcionarios)
+# Reajusta a lista de funcionários com os novos salários.
+lista_funcionarios_atualizado, total_aumento = funcoes.reajuste_salarial(lista_funcionarios)
+
+# Exibir os resultados.
+print("\n" + "="*40)
+print(f"Total geral de aumento: R${total_aumento:.2f}")
+print("\nO seguintes funcionários recebem menos de R$2000.00 depois do aumento salárial:")
+for nome, novo_salario in lista_funcionarios_atualizado:
+    if novo_salario < 2000:
+        print(f"- {nome}: R${novo_salario:.2f}")
+print("=" * 40)
